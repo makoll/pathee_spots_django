7a067db23c171e278a33d83756c0ba13cf823f24
Makoto Hirose <hirose@tritrue.com>
2019-02-04 00:28:42 +0900

    fix: Spots Tutorial 3

--- spots/urls.py
+++ spots/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'spots'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:spot_id>/', views.detail, name='detail'),
 ]
--- spots/views.py
+++ spots/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Spot
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    spot_list = Spot.objects.order_by('-id')[:5]
+    context = {'spot_list': spot_list}
+    return render(request, 'spots/index.html', context)
+
+
+def detail(request, spot_id):
+    spot = get_object_or_404(Spot, pk=spot_id)
+    return render(request, 'spots/detail.html', {'spot': spot})
